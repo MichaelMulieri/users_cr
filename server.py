a7ca30be41beea555ce1faf9b49c7bf76968c163
MichaelMulieri <mikemulieri@gmail.com>
2022-04-10 20:46:28 -0400

    User CRUD additions

--- server.py
+++ server.py
@@ -22,13 +22,35 @@ def create():
     User.save(request.form)
     return redirect('/users')
 
+@app.route('/user/show/<int:id>')
+def show_user(id):
+    data = {
+        "id":id
+    }
+    return render_template("show.html", user=User.show_one(data))
 
+@app.route('/user/edit/<int:id>')
+def edit(id):
+    data = {
+        "id":id
+    }
+    return render_template("edit_user.html", user=User.show_one(data))
 
+@app.route('/user/update', methods=['POST'])
+def update():
+    User.update(request.form)
+    return redirect('/users')
 
-
+@app.route('/user/delete/<int:id>')
+def delete(id):
+    data = {
+        "id":id
+    }
+    User.destroy(data)
+    return redirect('/users')
 
 
 
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug = True, port=5001)
--- user.py
+++ user.py
@@ -31,3 +31,19 @@ class User:
         query = "INSERT INTO users (first_name, last_name, email) VALUES (%(first_name)s, %(last_name)s, %(email)s);"
         result = connectToMySQL('user_schema').query_db(query, data)
         return result
+
+    @classmethod
+    def show_one(cls, data):
+        query = "SELECT * FROM users WHERE id = %(id)s;"
+        result = connectToMySQL('user_schema').query_db(query, data)
+        return cls(result[0])
+
+    @classmethod
+    def update(cls, data):
+        query = "UPDATE users SET first_name=%(first_name)s, last_name=%(last_name)s, email=%(email)s, updated_at=NOW() WHERE id = %(id)s;"
+        return connectToMySQL('user_schema').query_db(query, data)
+
+    @classmethod
+    def destroy(cls, data):
+        query = "DELETE FROM users WHERE id = %(id)s;"
+        return connectToMySQL('user_schema').query_db(query, data)
